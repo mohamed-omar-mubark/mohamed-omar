32c23a9941c5874c5e57ff0d33477a8db97c8348
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-12 16:09:39 +0200

    feat: Add skills view

--- src/components/main-views/Skills.vue
+++ src/components/main-views/Skills.vue
@@ -4,6 +4,42 @@
       <i class="far fa-address-card"></i>
       <span>Skills</span>
     </div>
+
+    <div class="view-subtitle">
+      <span class="d-block">Technologies and programming languages</span>
+    </div>
+
+    <p class="skill">
+      HTML, CSS, JavaScript,
+      <br>
+      CSS reprocessors (SCSS),
+      <br>
+      JavaScript frameworks & libraries (Vue.js - Jquery - React.js),
+      <br>
+      State management for vue app (Vuex - Pinia),
+      <br>
+      Vue-i18n,
+      <br>
+      Vue UI libraries (vuetify)
+      <br>
+      Version control system (Git),
+      <br>
+      Repo hosting services (Github),
+      <br>
+      Git workflow (Centralized workflow),
+      <br>
+      Package managers (npm - yarn),
+      <br>
+      JavaScript testing (Jest - Vue test utils),
+      <br>
+      Rest api (fetch - axios),
+      <br>
+      AJAX,
+      <br>
+      Build tools (Linters formatters (Prettier - ESLint)),
+      <br>
+      Desktop applications (Electron)
+    </p>
   </section>
 </template>
 
