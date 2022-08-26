449d357aa9150eaca15def7d303ea8fd3de84d51
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-08-26 19:26:57 +0200

    feat: update projects

--- src/components/main-views/Projects.vue
+++ src/components/main-views/Projects.vue
@@ -9,33 +9,20 @@
         <v-timeline-item color="teal lighten-3" small>
           <v-row class="pt-1">
             <v-col>
-              <span class="project-name d-block mb-1">Mo Panel</span>
-              <p class="project-caption">
-                Mo Panel is an admin dashboard template built with Vue 2, vuetify v2.6.7, Vue-i18n, Bootstrap v5.1.3.
-              </p>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
-
-        <v-timeline-item color="teal lighten-3" small>
-          <v-row class="pt-1">
-            <v-col>
-              <span class="project-name d-block mb-1">Shopping cart with vue</span>
-              <p class="project-caption">
-                Shopping cart with vue built with Vue 2, vuetify v2.6.7, Bootstrap v5.1.3,
-                <br>
-                Depend on localStorage to store, update quantity and delete item from cart.
-              </p>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
-
-        <v-timeline-item color="teal lighten-3" small>
-          <v-row class="pt-1">
-            <v-col>
               <span class="project-name d-block mb-1">360 Clinics</span>
               <p class="project-caption">
-                360 Clinics is an online clinics management system built with Vue 2 Bootstrap v5.1.3.
+                Multi tenant clinics management system built with Vue2, Vuetify and laravel backend.
+              </p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+
+        <v-timeline-item color="teal lighten-3" small>
+          <v-row class="pt-1">
+            <v-col>
+              <span class="project-name d-block mb-1">Vue shopping cart</span>
+              <p class="project-caption">
+                Shopping cart feature for ecommerce web apps, built with Vue2 and Bootstrap depends on browser localStorage.
               </p>
             </v-col>
           </v-row>
@@ -46,8 +33,52 @@
             <v-col>
               <span class="project-name d-block mb-1">Nafees</span>
               <p class="project-caption">
-                Nafees is an Ecommerce website to sell and buy watches, accessories and jewelry, with smart auction, shopping cart and wishlist.
-                <br>
+                Saudi ecoomerce web app you can buy luxurious watches, jewelry and antiques, built with Vue2, Vuex and laravel backend.
+              <br>
+              </p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+
+        <v-timeline-item color="teal lighten-3" small>
+          <v-row class="pt-1">
+            <v-col>
+              <span class="project-name d-block mb-1">Quasar todo</span>
+              <p class="project-caption">
+                Todo List App working on five different platforms ( Android, iOS, Mac, Windows & Web Browser ).
+              </p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+
+        <v-timeline-item color="teal lighten-3" small>
+          <v-row class="pt-1">
+            <v-col>
+              <span class="project-name d-block mb-1">Blue</span>
+              <p class="project-caption">
+                Real estate web app built with Vue 2, Vuetify.
+              </p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+
+        <v-timeline-item color="teal lighten-3" small>
+          <v-row class="pt-1">
+            <v-col>
+              <span class="project-name d-block mb-1">Mo panel</span>
+              <p class="project-caption">
+                Admin dashboard template built with Vue 2, Vuetify.
+              </p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+        
+        <v-timeline-item color="teal lighten-3" small>
+          <v-row class="pt-1">
+            <v-col>
+              <span class="project-name d-block mb-1">Node.js rest api with authentication</span>
+              <p class="project-caption">
+                Node.js rest api with authentication with mongodb.
               </p>
             </v-col>
           </v-row>
@@ -58,9 +89,7 @@
             <v-col>
               <span class="project-name d-block mb-1">Seddeq</span>
               <p class="project-caption">
-                Seddeq is an Ecommerce website built with Vue 2, vuetify v2.6.7, Bootstrap v5.1.3,
-                <br>
-                You can buy and sell products with Seddeq.
+                Ecommerce website built with Vue2, Vuex and Vuetify.
               </p>
             </v-col>
           </v-row>
@@ -71,7 +100,7 @@
             <v-col>
               <span class="project-name d-block mb-1">Albeshary</span>
               <p class="project-caption">
-                Albeshary is website of a leading company in the field of exporting and importing fruits and vegetables.
+                Website of a leading company in the field of exporting and importing fruits and vegetables, built with Vue2 and Bootstrap.
               </p>
             </v-col>
           </v-row>
